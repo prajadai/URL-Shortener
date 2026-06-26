da82e2b05cd24670c2202653e9e110e6e07195fa
Ayush Prajapati <ayushpjpt10@gmail.com>
2026-06-26 23:09:32 +0545

    added endpoints for stats and clicks, added click model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sqlmodel import Session, select
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 from database import create_db_and_tables, engine
-from models import Link, LinkBase, LinkCreate, LinkPublic
+from models import Link, LinkCreate, LinkPublic, Click
 import secrets
 
 app = FastAPI()
@@ -24,10 +24,35 @@ def create_shorten_url(link : LinkCreate):
         session.refresh(db_url)
         return db_url
     
+
+@app.get('/links', response_model=list[LinkPublic])
+def get_all_links():
+    with Session(engine) as session:
+        link = session.exec(select(Link)).all()
+        if link is None:
+            raise HTTPException(status_code=404, detail="No links found.")
+        return link
+    
+
+@app.get('/{short_code}/stats')
+def get_stats(short_code: str):
+    with Session(engine) as session:
+        link = session.exec(select(Link).where(Link.short_code == short_code)).first()
+        if not link:
+            raise HTTPException(status_code=404, detail="Code not found.")
+        stat = len(list(session.exec(select(Click).where(Click.link_id == link.id))))
+        if stat == 0:
+            return {"clicks": 0}
+        return {"clicks": stat}
+    
+
 @app.get('/{short_code}')
 def view_shorten_url(short_code: str):
     with Session(engine) as session:
         link = session.exec(select(Link).where(Link.short_code == short_code)).first()
         if not link:
             raise HTTPException(status_code=404, detail="Code not found.")
+        db_click = Click(link_id=link.id)
+        session.add(db_click)
+        session.commit()
         return RedirectResponse(url=link.original_url)
--- models.py
+++ models.py
@@ -17,3 +17,14 @@ class Link(LinkBase, table=True):
     id : int | None = Field(default=None, primary_key=True)
     short_code : str
     created_at : datetime = Field(default_factory=datetime.now)
+
+
+class User(SQLModel, table=True):
+    id : int | None = Field(default=None, primary_key=True)
+    username : str
+
+class Click(SQLModel, table=True):
+    id : int | None = Field(default=None, primary_key=True)
+    # user : str | None = Field(default=None, foreign_key="user.id")
+    link_id : int | None = Field(default=None, foreign_key="link.id")
+    clicked_at : datetime = Field(default_factory=datetime.now)
